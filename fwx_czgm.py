61c1a0f50dfd006fde90d54929e7a233eeb9b2f3
SeanSuny <20199755+SeanSuny@users.noreply.github.com>
2023-09-01 17:19:42 +0800

    Update

--- fwx_czgm.py
+++ fwx_czgm.py
@@ -362,5 +362,6 @@ if __name__ == '__main__':
     key = pushconfig['key']  # key从这里获取http://175.24.153.42:8882/getkey
     getmsg()
     for i in fwx_czgm:
+        print(f"\n===================当前第{fwx_czgm.index(i)+1}个账户===================")
         api = HHYD({'gfsessionid': i})
         api.run()
